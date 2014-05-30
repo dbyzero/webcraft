315a4744ab6878ed1e0048b0af310b6c8430335e
cyril <cyril@IWD-TOWER-011.IWD>
2014-05-30 19:42:22 +0200

    Adding blocks content on the bot right home page

--- js/webcraft.js
+++ js/webcraft.js
@@ -4,4 +4,24 @@ $(document).ready(function(){
 			value: 37
 		});
 	});
+	$( document ).tooltip({
+	  position: {
+		my: "center bottom",
+		at: "center top",
+		using: function( position, feedback ) {
+			$( this ).css( position );
+			$( "<div>" )
+				.addClass( "arrow" )
+				.addClass( feedback.vertical )
+				.addClass( feedback.horizontal )
+				.appendTo( this );
+			}
+		},
+		show: { 
+			duration: 0
+		},
+		hide: { 
+			duration: 0
+		}
+	});
 });
